c7bd40972c3fb3c035fefe524b740e016535092e
hnaufal-telkom <hnaufal@student.telkomuniversity.ac.id>
2025-03-17 00:28:38 +0700

    menambahkan class DataGeneric

--- tpmodul5_103022300163/Program.cs
+++ tpmodul5_103022300163/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+class DataGeneric<T> {
+	private T data;
+
+	public DataGeneric(ref T data) {
+		this.data = data;
+	}
+	public void PrintData() {
+		Console.WriteLine("Data yang tersimpan adalah: " + data);
+	}
+}
+
+class Program {
+	static void Main(string[] args) {
+		long nim = 103022300163;
+		DataGeneric<long> dataG = new DataGeneric<long>(ref nim);
+		dataG.PrintData();
+	}
+}
